a67388bc7853416003705cb00c65548f969d430d
deboer753 <myles.deboer@gmail.com>
2025-03-16 20:30:03 -0700

    take 3 and 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,17 @@ import Slider from "react-slick"; // Import react-slick
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const Arrow = ({ direction, onClick }) => (
+const Arrow = ({ direction, onClick }: { direction: "left" | "right"; onClick: () => void }) => (
   <button
     onClick={onClick}
-      className={`absolute top-1/2 transform -translate-y-1/2 bg-transparent backdrop-blur-md text-stone-700 p-3 z-10 mx-5 transition-all duration-300 hover:bg-white/10 text-2xl ${
+    className={`absolute top-1/2 transform -translate-y-1/2 bg-transparent backdrop-blur-md text-stone-700 p-3 z-10 mx-5 transition-all duration-300 hover:bg-white/10 text-2xl ${
       direction === "left" ? "left-[-50px]" : "right-[-50px]"
     }`}
   >
     {direction === "left" ? "←" : "→"}
   </button>
 );
+
 
 export default function Home() {
   const reviews = [
