c9c87611ebf68da412e75b07f49e87744bc6409d
deboer753 <myles.deboer@gmail.com>
2025-04-16 09:36:05 -0700

    more edits to email templat

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -19,10 +19,16 @@ export async function POST(req: Request) {
 
         <br/><br/>
         <div style="display: flex; align-items: center; gap: 10px; margin-top: 30px;">
-          <img src="https://graybuckmedia.com/imgs/graybuck_logo.png" alt="Graybuck Media Logo" style="border-radius: 6px; height: 50px; width: auto" />
+          <a href="https://graybuckmedia.com" target="_blank" style="display: inline-block;">
+            <img 
+              src="https://graybuckmedia.com/imgs/graybuck_logo.png" 
+              alt="Graybuck Media Logo" 
+              style="border-radius: 6px; height: 50px; width: auto; cursor: pointer;" 
+            />
+          </a>
           <p style="font-style: italic; color: #888; margin: 0;">
             Powered by Graybuck Media<br/>
-            Evolving creatively, protecting your vision | Web Design & Development
+            <span style="font-size: 12px;">Evolving creatively, protecting your vision | Web Design & Development</span>
           </p>
         </div>
       `,
