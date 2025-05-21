0b967f8fd90b5a6bcdb1c71577e9751298dd4ede
deboer753 <myles.deboer@gmail.com>
2025-05-21 08:44:55 -0700

    fixed contact button

--- src/app/route-contact/page.tsx
+++ src/app/route-contact/page.tsx
@@ -136,10 +136,10 @@ export default function Contact() {
             <div aria-live="polite">
               <button
                 type="submit"
-                className={`py-2 px-6 rounded font-bold transition-all focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                className={`py-2 px-6 rounded font-bold transition-all focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer ${
                   isSubmitted
-                    ? "bg-blue-500 text-white"
-                    : "bg-blue-600 text-white hover:bg-blue-700"
+                    ? "bg-blue-600 text-white"
+                    : "bg-blue-300 text-white hover:bg-blue-400"
                 }`}
                 disabled={isSubmitted}
               >
